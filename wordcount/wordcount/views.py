5979dc76de6ed6ecf9b876c29eb228cc2668294c
swarnimdixit <swarnim1234@gmail.com>
2019-04-21 14:44:44 +0530

    another first commit try

---
+++ wordcount/wordcount/views.py
@@ -0,0 +1,21 @@
+
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    if request.GET['fulltext'] != '':
+        fulltext = request.GET['fulltext'].strip()
+        if fulltext != '':
+            wordList = fulltext.split(' ')
+            wordCount = len(wordList)
+        else:
+            wordCount = 0
+    else:
+        wordCount = 0
+
+    return render(request, 'count.html', {'wordcount': '66'})
+
